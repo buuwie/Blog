4a118b864d3e5d33b8d375c867ba00b42b36277e
buuwie <hungtranngoc2002@gmail.com>
2023-11-23 02:14:55 +0700

    edit delete post clapped, dummy

--- Blog/Areas/Admin/Controllers/PostController.cs
+++ Blog/Areas/Admin/Controllers/PostController.cs
@@ -27,9 +27,10 @@ namespace Blog.Areas.Admin.Controllers
             _userManager = userManager;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            /*var listOfPosts = new List<Post>();
+            var listOfPosts = new List<Post>();
 
             var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
             var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
@@ -51,11 +52,7 @@ namespace Blog.Areas.Admin.Controllers
                 AuthorName = x.ApplicationUser!.FirstName + " " + x.ApplicationUser.LastName
             }).ToList();
 
-            int pageSize = 5;
-            int pageNumber = (page ?? 1);
-
-            return View(await listOfPostsVM.OrderByDescending(x => x.CreatedDate).ToPagedListAsync(pageNumber, pageSize));*/
-            return View();
+            return View(listOfPostsVM);
         }
 
         [HttpGet]
@@ -95,6 +92,79 @@ namespace Blog.Areas.Admin.Controllers
             await _context.SaveChangesAsync();
             _notification.Success("Post Created Successfully");
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var post = await _context.Posts!.FirstOrDefaultAsync(x => x.Id == id);
+
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+            var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
+
+            if (loggedInUserRole[0] == WebsiteRoles.WebsiteAdmin || loggedInUser?.Id == post?.ApplicationUserId)
+            {
+                _context.Posts!.Remove(post!);
+                await _context.SaveChangesAsync();
+                _notification.Success("Post Deleted Successfully");
+                return RedirectToAction("Index", "Post", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var post = await _context.Posts!.FirstOrDefaultAsync(x => x.Id == id);
+            if (post == null)
+            {
+                _notification.Error("Post not found");
+                return View();
+            }
+
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+            var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
+            if (loggedInUserRole[0] != WebsiteRoles.WebsiteAdmin && loggedInUser!.Id != post.ApplicationUserId)
+            {
+                _notification.Error("You are not authorized");
+                return RedirectToAction("Index");
+            }
+
+            var vm = new CreatePostVM()
+            {
+                Id = post.Id,
+                Title = post.Title,
+                ShortDescription = post.ShortDescription,
+                Description = post.Description,
+                ThumbnailUrl = post.ThumbnailUrl,
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CreatePostVM vm)
+        {
+            if (!ModelState.IsValid) { return View(vm); }
+            var post = await _context.Posts!.FirstOrDefaultAsync(x => x.Id == vm.Id);
+            if (post == null)
+            {
+                _notification.Error("Post not found");
+                return View();
+            }
+
+            post.Title = vm.Title;
+            post.ShortDescription = vm.ShortDescription;
+            post.Description = vm.Description;
+
+            if (vm.Thumbnail != null)
+            {
+                post.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+
+            await _context.SaveChangesAsync();
+            _notification.Success("Post updated succesfully");
+            return RedirectToAction("Index", "Post", new { area = "Admin" });
         }
 
         private string UploadImage(IFormFile file)
